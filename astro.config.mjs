8821d08a60a1146032f394807842d874f3237ad8
PrototypeArea <prototypearea.dev@gmail.com>
2022-06-25 05:53:08 -0400

    Create basic structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+  site: "https://tailwind-astro.vercel.app",
+
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    sitemap({}),
+  ],
 });
