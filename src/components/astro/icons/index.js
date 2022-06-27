2eac037090fc3ec44911185095e6fcc72844dcf9
PrototypeArea <prototypearea.dev@gmail.com>
2022-06-27 02:37:33 -0400

    Update icons and minimal details

---
+++ src/components/astro/icons/index.js
@@ -0,0 +1,15 @@
+import IconSun from "./IconSun.astro"
+import IconGithub from "./IconGithub.astro"
+import IconTwitter from "./IconTwitter.astro"
+import IconFacebook from "./IconFacebook.astro"
+import IconArrowRight from "./IconArrowRight.astro"
+import IconMenu from "./IconMenu.astro"
+
+export {
+  IconSun,
+  IconGithub,
+  IconTwitter,
+  IconFacebook,
+  IconArrowRight,
+  IconMenu
+}
