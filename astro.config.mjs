9d05f1aae889144745fa88ca99d121f6173c01d9
prototypa <prototypearea.dev@gmail.com>
2022-08-10 12:29:39 -0400

    Update site url in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  site: "https://tailwind-astro.vercel.app",
+  site: "https://astrowind.vercel.app/",
   integrations: [
     tailwind({
       config: {
