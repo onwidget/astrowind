65985c013f933d9da7393a036afb2a66e1274cce
prototypa <prototypearea.dev@gmail.com>
2022-08-10 19:10:37 -0400

    Update component classes to Tailwind CSS way

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ibm-plex-sans': ['"IBM Plex Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
