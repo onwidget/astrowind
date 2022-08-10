63e6ee9c05033686d9fe1e083b2101397414e729
prototypa <prototypearea.dev@gmail.com>
2022-08-10 14:19:08 -0400

    Optimize images with @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
   site: "https://astrowind.vercel.app/",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap({}),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap({}), image()]
 });
