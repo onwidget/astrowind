2cb0ccc04073fe655b6a73f501ee6c40e4ffa226
prototypa <prototypearea.dev@gmail.com>
2022-08-14 19:13:25 -0400

    Add icons to Basic Faqs

--- src/components/astro/icons/index.js
+++ src/components/astro/icons/index.js
@@ -5,13 +5,6 @@ import IconFacebook from "./IconFacebook.astro";
 import IconInstagram from "./IconInstagram.astro";
 import IconArrowRight from "./IconArrowRight.astro";
 import IconMenu from "./IconMenu.astro";
+import IconArrowDownRight from "./IconArrowDownRight.astro";
 
-export {
-  IconSun,
-  IconGithub,
-  IconTwitter,
-  IconFacebook,
-  IconInstagram,
-  IconArrowRight,
-  IconMenu,
-};
+export { IconSun, IconGithub, IconTwitter, IconFacebook, IconInstagram, IconArrowRight, IconMenu, IconArrowDownRight };
