48b636778b8f56b72a277b8bd67a125574e2895b
prototypa <prototypearea.dev@gmail.com>
2022-08-18 00:35:30 -0400

    Add RSS Icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -6,5 +6,16 @@ import IconInstagram from "./IconInstagram.astro";
 import IconArrowRight from "./IconArrowRight.astro";
 import IconMenu from "./IconMenu.astro";
 import IconArrowDownRight from "./IconArrowDownRight.astro";
+import IconRSS from "./IconRSS.astro";
 
-export { IconSun, IconGithub, IconTwitter, IconFacebook, IconInstagram, IconArrowRight, IconMenu, IconArrowDownRight };
+export {
+  IconSun,
+  IconGithub,
+  IconTwitter,
+  IconFacebook,
+  IconInstagram,
+  IconArrowRight,
+  IconMenu,
+  IconArrowDownRight,
+  IconRSS,
+};
