fc83e9864bb1758779f0e30d41d06b764ff9a491
Monica Leiras <monicarafaelaleiras@gmail.com>
2022-11-16 11:41:18 +0000

    First version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,8 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				primary: colors.orange,
+				secondary: colors.red,
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
