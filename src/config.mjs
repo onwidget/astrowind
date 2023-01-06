ba2b6081ed290d4ac75e054b89d334f5ac559bab
prototypa <prototypearea.dev@gmail.com>
2023-01-06 15:25:11 -0500

    Add new 'noindex' settings for Blog entitities

--- src/config.mjs
+++ src/config.mjs
@@ -24,20 +24,24 @@ export const BLOG = {
 	blog: {
 		disabled: false,
 		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+		noindex: false,
 	},
 
 	post: {
 		disabled: false,
 		pathname: '', // empty for /some-post, value for /pathname/some-post
+		noindex: false,
 	},
 
 	category: {
 		disabled: false,
 		pathname: 'category', // set empty to change from /category/some-category to /some-category
+		noindex: true,
 	},
 
 	tag: {
 		disabled: false,
 		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+		noindex: true,
 	},
 };
