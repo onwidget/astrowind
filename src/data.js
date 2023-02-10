e39aadd7f729f633f05bc7ee293cde61fc3aea09
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2023-02-10 23:43:59 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,57 +1,6 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-  links: [
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Sass',
-          href: '#',
-        },
-        {
-          text: 'Startup',
-          href: '#',
-        },
-        {
-          text: 'Mobile App',
-          href: '#',
-        },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features',
-          href: '#',
-        },
-        {
-          text: 'Pricing',
-          href: '#',
-        },
-        {
-          text: 'About us',
-          href: '#',
-        },
-        {
-          text: 'Contact',
-          href: '#',
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
     {
       text: 'Blog',
       href: getBlogPermalink(),
