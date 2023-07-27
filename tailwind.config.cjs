8a4999cd6509a737b9d40ee6284ed402d6dd3b36
prototypa <prototypearea.dev@gmail.com>
2023-07-27 14:06:54 -0400

    Add new custom color 'muted'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
         primary: 'var(--aw-color-primary)',
         secondary: 'var(--aw-color-secondary)',
         accent: 'var(--aw-color-accent)',
+        muted: 'var(--aw-color-muted)',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
