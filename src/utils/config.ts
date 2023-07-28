12f46121c481832b01636fc792fa22c50d7c1e96
prototypa <prototypearea.dev@gmail.com>
2023-07-27 21:49:32 -0400

    Add support for config in yaml

---
+++ src/utils/config.ts
@@ -0,0 +1,204 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+import merge from 'lodash.merge';
+
+import type { MetaSEO } from '~/types';
+
+export interface SiteConfig {
+  name: string;
+  site?: string;
+  base?: string;
+  trailingSlash?: boolean;
+  googleSiteVerificationId?: string;
+}
+export interface MetaDataConfig extends Omit<MetaSEO, 'title'> {
+  title?: {
+    default: string;
+    template: string;
+  };
+}
+export interface I18NConfig {
+  language: string;
+  textDirection: string;
+  dateFormatter: unknown;
+}
+export interface AppBlogConfig {
+  isEnabled: boolean;
+  postsPerPage: number;
+  post: {
+    isEnabled: boolean;
+    permalink: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  list: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  category: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  tag: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+}
+export interface AnalyticsConfig {
+  vendors: {
+    googleAnalytics: {
+      isEnabled?: boolean;
+      id?: string;
+    };
+  };
+}
+
+const config = yaml.load(fs.readFileSync('src/config.yaml', 'utf8')) as {
+  site?: SiteConfig;
+  metadata?: MetaDataConfig;
+  i18n?: I18NConfig;
+  apps?: {
+    blog?: AppBlogConfig;
+  };
+  ui?: unknown;
+  analytics?: unknown;
+};
+
+const DEFAULT_SITE_NAME = 'Website';
+
+const getSite = () => {
+  const _default = {
+    name: DEFAULT_SITE_NAME,
+    site: undefined,
+    base: '/',
+    trailingSlash: false,
+
+    googleSiteVerificationId: '',
+  };
+
+  return merge({}, _default, config?.site ?? {}) as SiteConfig;
+};
+
+const getMetadata = () => {
+  const siteConfig = getSite();
+
+  const _default = {
+    title: {
+      default: siteConfig?.name || DEFAULT_SITE_NAME,
+      template: '%s',
+    },
+    description: '',
+    robots: {
+      index: false,
+      follow: false,
+    },
+    openGraph: {
+      type: 'website',
+    },
+  };
+
+  return merge({}, _default, config?.metadata ?? {}) as MetaDataConfig;
+};
+
+const getI18N = () => {
+  const _default = {
+    language: 'en',
+    textDirection: 'ltr',
+  };
+
+  const value = merge({}, _default, config?.i18n ?? {});
+
+  return Object.assign(value, {
+    dateFormatter: new Intl.DateTimeFormat(value.language, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      timeZone: 'UTC',
+    }),
+  }) as I18NConfig;
+};
+
+const getAppBlog = () => {
+  const _default = {
+    isEnabled: false,
+    postsPerPage: 6,
+    post: {
+      isEnabled: true,
+      permalink: '/blog/%slug%',
+      robots: {
+        index: true,
+        follow: true,
+      },
+    },
+    list: {
+      isEnabled: true,
+      pathname: 'blog',
+      robots: {
+        index: true,
+        follow: true,
+      },
+    },
+    category: {
+      isEnabled: true,
+      pathname: 'category',
+      robots: {
+        index: true,
+        follow: true,
+      },
+    },
+    tag: {
+      isEnabled: true,
+      pathname: 'tag',
+      robots: {
+        index: false,
+        follow: true,
+      },
+    },
+  };
+
+  return merge({}, _default, config?.apps?.blog ?? {}) as AppBlogConfig;
+};
+
+const getUI = () => {
+  const _default = {
+    theme: 'system',
+    classes: {},
+    tokens: {},
+  };
+
+  return merge({}, _default, config?.ui ?? {});
+};
+
+const getAnalytics = () => {
+  const _default = {
+    vendors: {
+      googleAnalytics: {
+        isEnabled: false,
+        id: undefined,
+      },
+    },
+  };
+
+  return merge({}, _default, config?.analytics ?? {}) as AnalyticsConfig;
+};
+
+export const SITE_CONFIG = getSite();
+export const I18N_CONFIG = getI18N();
+export const METADATA_CONFIG = getMetadata();
+export const APP_BLOG_CONFIG = getAppBlog();
+export const UI_CONFIG = getUI();
+export const ANALYTICS_CONFIG = getAnalytics();
