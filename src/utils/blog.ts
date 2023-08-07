3ef024f14982556367cdb3b1b19b5cc580e6b819
prototypa <prototypearea.dev@gmail.com>
2023-08-07 08:21:02 -0400

    Allow child posts (or sub posts)

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -56,7 +56,7 @@ const getNormalizedPost = async (post: CollectionEntry<'post'>): Promise<Post> =
     metadata = {},
   } = data;
 
-  const slug = cleanSlug(rawSlug.split('/').pop());
+  const slug = cleanSlug(rawSlug); // cleanSlug(rawSlug.split('/').pop());
   const publishDate = new Date(rawPublishDate);
   const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
   const category = rawCategory ? cleanSlug(rawCategory) : undefined;
