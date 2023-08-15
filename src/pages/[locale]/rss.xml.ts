84202bf34e91e560ff23db0315f4f8a765636291
reuseman <alexcolucci@protonmail.com>
2023-08-15 11:32:07 +0200

    Add localized rss and fix build bug with proper response

--- src/pages/[locale]/rss.xml.ts
+++ src/pages/[locale]/rss.xml.ts
@@ -1,10 +1,23 @@
+import type { APIRoute } from 'astro';
 import rss from '@astrojs/rss';
 
 import { SITE_CONFIG, METADATA_CONFIG, APP_BLOG_CONFIG, I18N_CONFIG } from '~/utils/config';
 import { fetchPosts } from '~/utils/blog';
 import { getPermalink } from '~/utils/permalinks';
 
-export const get = async () => {
+// i18n RSS feed
+
+export function getStaticPaths() {
+  if (!APP_BLOG_CONFIG.isEnabled) {
+    return [];
+  }
+
+  return Object.keys(I18N_CONFIG.locales).map((locale) => ({ params: { locale } }));
+}
+
+export const get: APIRoute = async function get({ params, redirect }) {
+  const locale = params.locale || I18N_CONFIG.defaultLocale;
+
   if (!APP_BLOG_CONFIG.isEnabled) {
     return new Response(null, {
       status: 404,
@@ -12,11 +25,22 @@ export const get = async () => {
     });
   }
 
-  const posts = await fetchPosts(I18N_CONFIG.defaultLocale);
+  if (locale === I18N_CONFIG.defaultLocale) {
+    return redirect('/rss.xml');
+  }
+
+  const posts = await fetchPosts(locale);
+
+  if (posts.length === 0) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not Found',
+    });
+  }
 
   const { body } = await rss({
     title: `${SITE_CONFIG.name}’s Blog`,
-    description: METADATA_CONFIG?.description || "",
+    description: METADATA_CONFIG?.description || '',
     site: import.meta.env.SITE,
 
     items: posts.map((post) => ({
@@ -25,12 +49,10 @@ export const get = async () => {
       description: post.excerpt,
       pubDate: post.publishDate,
     })),
-
-    trailingSlash: SITE_CONFIG.trailingSlash,
   });
 
   return new Response(body, {
     status: 200,
-    statusText: "OK",
+    statusText: 'OK',
   });
 };
