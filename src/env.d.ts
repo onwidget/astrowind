95899486db8d421808abb9a94f69f5801cbb8768
prototypa <prototypearea.dev@gmail.com>
2023-08-25 15:32:19 -0400

    Update to astro@3.0.0-rc.7

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
+/// <reference types="astro/client" />
 /// <reference types="vite/client" />
