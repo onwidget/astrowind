d26968205514194d705db0a57cd14f76cd69396a
uxbycarol <carolain.hernandez27605@gmail.com>
2024-02-19 10:53:25 -0400

    jugando con la vaina

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    
     extend: {
       colors: {
         primary: 'var(--aw-color-primary)',
@@ -15,6 +16,14 @@ module.exports = {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xl: '1.25rem',
+        '2xl': 'clamp(1rem, 1.5vw, 1.75rem)',
+        '3xl': 'clamp(1.25rem, 2vw, 2rem)',
+        '4xl': 'clamp(1.75rem, 2.5vw, 2.5rem)',
+        '5xl': 'clamp(2rem, 3vw, 3.25rem)',
+        '6xl': 'clamp(2.5rem, 3.5vw, 3.5rem)',
       },
     },
   },
