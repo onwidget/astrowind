e224f832c0029fc1dcf4efd9e526924184c01064
prototypa <prototypearea.dev@gmail.com>
2024-03-02 04:18:40 -0500

    Fix Lighthouse A11y errors on dark mode

--- src/navigation.js
+++ src/navigation.js
@@ -178,6 +178,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
   `,
 };
