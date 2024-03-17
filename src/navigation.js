73bbe01a5fc25756e32082dd2566f1fd8bdc2ac0
Gunther Brunner <gunta85@gmail.com>
2024-03-18 06:31:25 +0900

    Customize with Bun to GP

--- src/navigation.js
+++ src/navigation.js
@@ -177,7 +177,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"></span>
+    Copyright <a class="text-blue-600 underline dark:text-muted" href="https://g-productions.studio/"> G Productions</a> · All rights reserved.
   `,
 };
