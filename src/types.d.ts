356854f018b9068331d3570687f52e4c1a1059ba
Dylan Awalt-Conley <web@dylan.ac>
2024-03-26 12:08:27 -0400

    fix: CallToAction interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -176,7 +176,7 @@ export interface Disclaimer {
 }
 
 // COMPONENTS
-export interface CallToAction extends HTMLAttributes<a> {
+export interface CallToAction extends Omit<HTMLAttributes<'a'>, 'slot'> {
   variant?: 'primary' | 'secondary' | 'tertiary' | 'link';
   text?: string;
   icon?: string;
