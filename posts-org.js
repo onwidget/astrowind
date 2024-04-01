4ada99d3af4a7b06593098db45ef3a764e8a9627
Diter Evan <hansevangelista@gmail.com>
2024-04-01 15:57:23 -0500

    parse notion blocks

--- posts-org.js
+++ posts-org.js
@@ -48,21 +48,20 @@ async function main() {
     _results.forEach(block => {
 
       if(block.type == 'heading_1') {
+
         const heading_1 = block.heading_1.rich_text[0].text.content
-        // console.log({ heading_1 })
+        console.log({ heading_1 })
+
+
         body += "\n" + `## ${heading_1}`
       }
 
-      if(block.type == 'heading_2') {
-        const heading_2 = block.heading_2.rich_text[0].text.content
-        // console.log({ heading_2 })
-        body += "\n" + `## ${heading_2}`
-      }
-
       if(block.type == 'paragraph') {
-        const paragraph = block.paragraph.rich_text[0]?.text.content
-        // console.log({ paragraph })
-        body += "\n" + `${paragraph || ''}`
+
+        const paragraph = block.paragraph.rich_text[0].text.content
+        console.log({ paragraph })
+
+        body += "\n" + `${paragraph}`
       }
     })
 
@@ -102,7 +101,26 @@ tags: ${_Tags}
 metadata:
   canonical: https://astrowind.vercel.app/get-started-website-with-astro-tailwind-css
 ---
-${body}`
+
+${body}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## Nostra torquent consequat volutpat aliquet neque
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit proin, aenean litora volutpat urna egestas magnis arcu non, cras ut cursus et sed morbi lectus. Integer faucibus sagittis eu nunc urna aliquet a laoreet torquent, suspendisse penatibus nulla sollicitudin congue rutrum dictum. Ornare mi habitasse fermentum phasellus dui et morbi litora sodales dictum id erat, nibh purus class ligula aenean lectus venenatis euismod cras torquent ac. Senectus sagittis conubia hendrerit at egestas porta venenatis nisi metus gravida tempor, aenean facilisis nisl ante facilisi lacus integer hac iaculis purus. Scelerisque libero torquent egestas curae tellus viverra inceptos imperdiet urna, porta suspendisse interdum primis odio morbi tempor commodo dictumst, suscipit ornare habitasse semper feugiat cras quisque lobortis.
+
+## Praesent tellus ad sapien erat or
+
+- Quam orci nostra mi nulla, hac a.
+
+- Interdum iaculis quis tellus sociis orci nulla, quam rutrum conubia tortor primis.
+
+- Non felis sem placerat aenean duis, ornare turpis nostra.
+
+- Habitasse duis sociis sagittis cursus, ante dictumst commodo.
+
+Duis maecenas massa habitasse inceptos imperdiet scelerisque at condimentum ultrices, nam dui leo enim taciti varius cras habitant pretium rhoncus, ut hac euismod nostra metus sagittis mi aenean. Quam eleifend aliquet litora eget a tempor, ultricies integer vestibulum non felis sodales, eros diam massa libero iaculis.`
 
     fs.writeFile(process.cwd() + `/src/content/post/${page.id}.md`, content, err => {
       if (err) {
---
+++ src/content/post/6c7bf778-1632-42ee-bddd-d0ae0d3ab1ae.md
@@ -0,0 +1,18 @@
+---
+publishDate: 2024-03-27
+author: Diter Evan
+title: Title
+excerpt: Excerpt
+image: https://t4.ftcdn.net/jpg/05/57/08/33/360_F_557083387_i4zwxJNVLZsCFzYLubwfYoiI5YjBQk4Y.jpg
+category: Tutorials
+tags: 
+  - tag
+metadata:
+  canonical: https://astrowind.vercel.app/get-started-website-with-astro-tailwind-css
+---
+
+## Este es un titulo
+Párrafo estándar
+
+## Titulo 2
+Texto numero 2
