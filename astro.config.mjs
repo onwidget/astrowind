eca94988d6a42a81194897c695d90b4a4ffc5cf2
starraiderx <julius@bairaktaris.de>
2024-04-09 01:26:57 +0200

    Switch to @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
-import compress from 'astro-compress';
+import compress from '@playform/compress';
 
 import astrowind from './src/integration';
 
