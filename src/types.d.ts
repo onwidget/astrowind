a606b34a194d90057bb55503aea33d1419d9e1d4
prototypa <prototypearea.dev@gmail.com>
2024-04-29 17:51:55 -0400

    Format all files with prettier: npm run format

--- src/types.d.ts
+++ src/types.d.ts
@@ -24,7 +24,7 @@ export interface Post {
   image?: ImageMetadata | string;
 
   /**  */
-  category?: Taxonomy
+  category?: Taxonomy;
   /**  */
   tags?: Taxonomy[];
   /**  */
@@ -213,7 +213,7 @@ export interface Form {
 }
 
 // WIDGETS
-export interface Hero extends Omit<Headline,"classes">, Widget {
+export interface Hero extends Omit<Headline, 'classes'>, Widget {
   content?: string;
   image?: string | unknown;
   callToAction1?: CallToAction;
@@ -221,29 +221,29 @@ export interface Hero extends Omit<Headline,"classes">, Widget {
   isReversed?: boolean;
 }
 
-export interface Team extends Omit<Headline,"classes">, Widget {
+export interface Team extends Omit<Headline, 'classes'>, Widget {
   team?: Array<TeamMember>;
 }
 
-export interface Stats extends Omit<Headline,"classes">, Widget {
+export interface Stats extends Omit<Headline, 'classes'>, Widget {
   stats?: Array<Stat>;
 }
 
-export interface Pricing extends Omit<Headline,"classes">, Widget {
+export interface Pricing extends Omit<Headline, 'classes'>, Widget {
   prices?: Array<Price>;
 }
 
-export interface Testimonials extends Omit<Headline,"classes">, Widget {
+export interface Testimonials extends Omit<Headline, 'classes'>, Widget {
   testimonials?: Array<Testimonial>;
   callToAction?: CallToAction;
 }
 
-export interface Brands extends Omit<Headline,"classes">, Widget {
+export interface Brands extends Omit<Headline, 'classes'>, Widget {
   icons?: Array<string>;
   images?: Array<Image>;
 }
 
-export interface Features extends Omit<Headline,"classes">, Widget {
+export interface Features extends Omit<Headline, 'classes'>, Widget {
   image?: string | unknown;
   video?: Video;
   items?: Array<Item>;
@@ -256,14 +256,14 @@ export interface Features extends Omit<Headline,"classes">, Widget {
   isAfterContent?: boolean;
 }
 
-export interface Faqs extends Omit<Headline,"classes">, Widget {
+export interface Faqs extends Omit<Headline, 'classes'>, Widget {
   iconUp?: string;
   iconDown?: string;
   items?: Array<Item>;
   columns?: number;
 }
 
-export interface Steps extends Omit<Headline,"classes">, Widget {
+export interface Steps extends Omit<Headline, 'classes'>, Widget {
   items: Array<{
     title: string;
     description?: string;
@@ -275,7 +275,7 @@ export interface Steps extends Omit<Headline,"classes">, Widget {
   isReversed?: boolean;
 }
 
-export interface Content extends Omit<Headline,"classes">, Widget {
+export interface Content extends Omit<Headline, 'classes'>, Widget {
   content?: string;
   image?: string | unknown;
   items?: Array<Item>;
@@ -285,4 +285,4 @@ export interface Content extends Omit<Headline,"classes">, Widget {
   callToAction?: CallToAction;
 }
 
-export interface Contact extends Omit<Headline,"classes">, Form, Widget {}
+export interface Contact extends Omit<Headline, 'classes'>, Form, Widget {}
--- vendor/integration/utils/loadConfig.ts
+++ vendor/integration/utils/loadConfig.ts
@@ -4,7 +4,7 @@ import yaml from 'js-yaml';
 const loadConfig = async (configPathOrData: string | object) => {
   if (typeof configPathOrData === 'string') {
     const content = fs.readFileSync(configPathOrData, 'utf8');
-    if (configPathOrData.endsWith(".yaml") || configPathOrData.endsWith(".yml")) {
+    if (configPathOrData.endsWith('.yaml') || configPathOrData.endsWith('.yml')) {
       return yaml.load(content);
     }
     return content;
