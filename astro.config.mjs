bc48a2bcb68e2f6044856495ad46964866322bc8
prototypa <prototypearea.dev@gmail.com>
2024-08-25 23:54:24 -0400

    Remove deprecated squooshImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -75,7 +75,6 @@ export default defineConfig({
   ],
 
   image: {
-    service: squooshImageService(),
     domains: ['cdn.pixabay.com'],
   },
 
