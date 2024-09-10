51cef74e12cdcf0fa456a1dec31f12a5b5879e3a
skeptrune <nick.k@trieve.ai>
2024-09-10 15:35:09 -0700

    cleanup: change default trailSlash behavior to ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const whenExternalScripts = (items = []) =>
 export default defineConfig({
   site: SITE.site,
   base: SITE.base,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+  trailingSlash: SITE.trailingSlash ? 'always' : 'ignore',
 
   output: 'static',
 
