988b6f5a3bb29f7f3d06e14fa29c7bcd5b48742f
skeptrune <nick.k@trieve.ai>
2024-10-29 21:14:03 -0700

    feature: add new logos and fix search component page positioning

--- src/components/widgets/SearchSmall.jsx
+++ src/components/widgets/SearchSmall.jsx
@@ -11,10 +11,14 @@ const SearchComponentSmall = () => {
       brandLogoImgSrcUrl="https://cdn.trieve.ai/trieve-logo.png"
       brandName="Trieve"
       brandColor="#b557c5"
-      problemLink='mailto:help@trieve.ai?subject='
       searchOptions={{
         use_autocomplete: false,
+        typo_options: {
+          correct_typos: true,
+        },
+        search_type: 'fulltext',
       }}
+      problemLink="mailto:help@trieve.ai?subject="
       tags={[
         {
           key: 'openapi-route',
@@ -23,52 +27,13 @@ const SearchComponentSmall = () => {
           icon: () => (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+              width="16"
+              height="16"
+              fill="currentColor"
               className="w-3 h-3"
+              viewBox="0 0 16 16"
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-              <path d="M9 14v.01" />
-              <path d="M12 14v.01" />
-              <path d="M15 14v.01" />
-            </svg>
-          ),
-        },
-        {
-          key: 'blog',
-          tag: 'blog',
-          label: 'Blog',
-          icon: () => (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-3 h-3"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M22 12.54c-1.804 -.345 -2.701 -1.08 -3.523 -2.94c-.487 .696 -1.102 1.568 -.92 2.4c.028 .238 -.32 1 -.557 1h-14c0 5.208 3.164 7 6.196 7c4.124 .022 7.828 -1.376 9.854 -5c1.146 -.101 2.296 -1.505 2.95 -2.46z" />
-              <path d="M5 10h3v3h-3z" />
-              <path d="M8 10h3v3h-3z" />
-              <path d="M11 10h3v3h-3z" />
-              <path d="M8 7h3v3h-3z" />
-              <path d="M11 7h3v3h-3z" />
-              <path d="M11 4h3v3h-3z" />
-              <path d="M4.571 18c1.5 0 2.047 -.074 2.958 -.78" />
-              <path d="M10 16l0 .01" />
+              <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0" />
             </svg>
           ),
         },
