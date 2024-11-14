5f83f713c0ea7eafd9bc538a2deec8e269f5c64d
Ignacio98 <dani.manchu1998@gmail.com>
2024-11-14 18:22:38 -0500

    Publicando para presentar

--- src/navigation.ts
+++ src/navigation.ts
@@ -15,7 +15,7 @@ export const headerData = {
         },
         {
           text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          href: getPermalink('/'),
         },
         {
           text: 'Personal',
@@ -121,52 +121,33 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Productos',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Aplicación web', href: 'https://app.acontplus.com/Index' },
+        { text: 'POS AcontPlus', href: 'https://restaurant.acontplus.com/auth' },
       ],
     },
     {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
+      title: 'App Facturación',
+      links: [{ text: 'Acerca de nosotros', href: '#' }],
     },
     {
-      title: 'Support',
+      title: 'Soporte',
       links: [
         { text: 'Docs', href: '#' },
         { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
       ],
     },
     {
-      title: 'Company',
+      title: 'AcontPlus',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Servicios', href: 'https://acontplus.com.ec/servicios/' },
+        { text: 'Clientes', href: 'https://acontplus.com.ec/clientes/' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Terms', href: getPermalink('/politicas-de-privacidad-y-proteccion-de-datos-personales') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
@@ -177,7 +158,10 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"
+        src="https://onwidget.com/favicon/favicon-32x32.png"
+        alt="onWidget logo" loading="lazy">
+    </img>
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com"> onWidget</a> & <a class="text-blue-600 underline dark:text-muted" href="https://acontplus.com.ec"> AcontPlus</a> · Todos los derechos reservados.
   `,
 };
