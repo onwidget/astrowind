e045a294fdc0a431b687cefae295d2a4aae2ca3f
Ignacio98 <dani.manchu1998@gmail.com>
2024-11-14 13:23:58 -0500

    Avances en modificaciones.

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -87,7 +87,7 @@ export const getPermalink = (slug = '', type = 'page'): string => {
 };
 
 /** */
-export const getHomePermalink = (): string => getPermalink('/');
+export const getHomePermalink = (): string => getPermalink('/homes/mobile-app');
 
 /** */
 export const getBlogPermalink = (): string => getPermalink(BLOG_BASE);
