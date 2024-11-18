97d959fe94ef3f95925788023abb9d0b00f181da
Ignacio98 <dani.manchu1998@gmail.com>
2024-11-18 09:57:43 -0500

    Update links and remove images.

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,4 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -123,13 +123,14 @@ export const footerData = {
     {
       title: 'Productos',
       links: [
-        { text: 'Aplicación web', href: 'https://app.acontplus.com/Index' },
+        { text: 'AcontPlus Web', href: 'https://app.acontplus.com/Index' },
+        { text: 'AcontPlus Web Demo', href: 'https://demo.acontplus.com/register' },
         { text: 'POS AcontPlus', href: 'https://restaurant.acontplus.com/auth' },
       ],
     },
     {
       title: 'App Facturación',
-      links: [{ text: 'Acerca de nosotros', href: '#' }],
+      links: [{ text: 'Acerca de nosotros', href: '#about' }],
     },
     {
       title: 'Soporte',
@@ -151,11 +152,9 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/acontplus.ec' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/acontplus.ec' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/acontplus-sas' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm"
