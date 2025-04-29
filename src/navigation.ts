bde27bbf3f5e278792706fa7c378962ce5aedd8f
Neeraj singh <nirmalapanghal43@gmail.com>
2025-04-29 11:59:26 +0530

    Restore previous commit that was reset

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,6 +6,15 @@ export const headerData = {
       text: 'Blog',
       href: getBlogPermalink()
     },
+
+    {
+      text: 'Projects',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Resume',
+      href: getPermalink('/resume'),
+    },
     {
       text: 'Pages',
       links: [
@@ -86,14 +95,14 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/neerajlovecyber' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/neerajsinghpanghal/' },
+    { ariaLabel: 'linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/neerajlovecyber/' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/neerajlovecyber' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    💖
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://neerajlovecyber.com/"> Neerajlovecyber</a> · All rights reserved.
   `,
 };
