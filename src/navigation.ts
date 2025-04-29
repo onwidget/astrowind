3a311a17d17262fe73519e7b357bccfa67f97a71
Neeraj singh <nirmalapanghal43@gmail.com>
2025-04-29 17:52:45 +0530

    asd

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,6 +3,10 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
+    {
       text: 'Blog',
       href: getBlogPermalink(),
     },
