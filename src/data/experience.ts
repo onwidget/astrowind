744182e92a863e0480c978ca90611e98507a90ac
Neeraj singh <nirmalapanghal43@gmail.com>
2025-05-31 12:04:17 +0530

    experinces

---
+++ src/data/experience.ts
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    title: 'Cyber Security Intern',
+    company: 'xIoTz, Bengaluru',
+    date: 'November 2024 - Present',
+    description:
+      'Contributed to the development of xIoTz eXtended Operation Centre (XOC), a unified cyber assurance platform integrating NOC, SOC, TOC, CSPM, and NGVA. Worked on automated threat Detection & responses and integrated Wazuh for security monitoring.',
+    isLatest: true,
+  },
+  {
+    title: 'SDET Intern',
+    company: 'Frugal Testing, Hyderabad',
+    date: 'Oct 2023 - Sep 2024',
+    description:
+      'Automated web/API testing using Java, Selenium, and Postman. Improved CI/CD with GitHub Actions. Used JIRA, Trello, and Allure for reporting and defect tracking.',
+    isLatest: false,
+  },
+  {
+    title: 'Cyber Security Intern',
+    company: 'Gurugram Police',
+    date: 'Jun 2021 - Jul 2021',
+    description:
+      'Worked on real-world cases in bug hunting, digital forensics, OSINT, and cyber law. Performed penetration testing with tools like Nmap, BurpSuite, and Metasploit.',
+    isLatest: false,
+  },
+];
