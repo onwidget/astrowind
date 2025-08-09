d9eb17573b55c7017e29c56cb4e2b323a56d81bf
André B <54446028+prototypa@users.noreply.github.com>
2025-08-09 09:29:30 -0400

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -177,7 +177,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/onwidget"> onWidget</a> · All rights reserved.
   `,
 };
